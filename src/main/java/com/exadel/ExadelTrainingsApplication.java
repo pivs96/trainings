c1776cf2d3f24128e93c837a75854333281a833f
Konstantin Marmalyukov <kmarmaliykov@exadel.com>
2015-07-09 14:05:29 +0300

    Initial maven app

---
+++ src/main/java/com/exadel/ExadelTrainingsApplication.java
@@ -0,0 +1,12 @@
+package com.exadel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExadelTrainingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExadelTrainingsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/exadel/ExadelTrainingsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.exadel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ExadelTrainingsApplication.class)
+@WebAppConfiguration
+public class ExadelTrainingsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
