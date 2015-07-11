416174108e729a1095b51d671a3196f3c608fdf0
Natallia Kiraikova <n.kozlova@gmail.com>
2015-07-11 20:05:30 +0300

    Draft ccommit

---
+++ frontend/app/scripts/controllers/header.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Natsik on 11.07.2015.
+ */
+angular.module('frontendApp')
+  .controller('HeaderCtrl', function () {
+   // this.companyName = companyName;
+  });
+
+var companyName = "Exadel";
