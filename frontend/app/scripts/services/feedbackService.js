c18d1f295c052b3d33d62d3b330fc2219b7ad1c3
IPoyda <rock.ivan.poyda@gmail.com>
2015-07-16 22:59:15 +0300

    parsed trainer in trainingList. added post request in feedback controller

---
+++ frontend/app/scripts/services/feedbackService.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/*angular.module('frontendApp')
+  .factory('FeedbackService',['$resource','data', function($resource,data) {
+    return $resource('http://localhost:8080/feedback', {}, {
+      postFeedback: {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        data: data
+      }
+    });
+  }]);*/
