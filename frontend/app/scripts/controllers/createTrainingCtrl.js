864c7b4f683933850d5f6620430232c781745981
Demengjkee <ncherepkin@live.com>
2015-07-28 16:00:02 +0300

    added html for createTrainingPage + directive for it

--- frontend/app/scripts/controllers/createTrainingCtrl.js
+++ frontend/app/scripts/controllers/createTrainingCtrl.js
@@ -1,70 +1,21 @@
 'use strict';
 
 angular.module('frontendApp')
-  .controller('CreateTrainingCtrl', ['$scope', function($scope) {
+  .controller('CreateTrainingCtrl', ['$scope', '$compile', function($scope, $compile) {
+
+    $scope.data = {};
 
 
-
-    $scope.today = function() {
-      $scope.dt = new Date();
-    };
-    $scope.today();
-
-    $scope.clear = function () {
-      $scope.dt = null;
+    $scope.addEntry = function($event) {
+      //TODO: make this work
+      console.log(angular.element('.col-sm-7')[2].append($compile('<training-date-select></training-date-select>')));
     };
 
+    $scope.repeating = false;
 
+    $scope.days = ['Monday','Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-    $scope.toggleMin = function() {
-      $scope.minDate = $scope.minDate ? null : new Date();
-    };
-    $scope.toggleMin();
-
-    $scope.open = function($event) {
-      $event.preventDefault();
-      $event.stopPropagation();
-
-      $scope.opened = true;
-    };
-
-    $scope.dateOptions = {
-      formatYear: 'yy',
-      startingDay: 1
-    };
-
-    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-    $scope.format = $scope.formats[0];
-
-    var tomorrow = new Date();
-    tomorrow.setDate(tomorrow.getDate() + 1);
-    var afterTomorrow = new Date();
-    afterTomorrow.setDate(tomorrow.getDate() + 2);
-    $scope.events =
-      [
-        {
-          date: tomorrow,
-          status: 'full'
-        },
-        {
-          date: afterTomorrow,
-          status: 'partially'
-        }
-      ];
-
-    $scope.getDayClass = function(date, mode) {
-      if (mode === 'day') {
-        var dayToCheck = new Date(date).setHours(0,0,0,0);
-
-        for (var i=0;i<$scope.events.length;i++){
-          var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
-
-          if (dayToCheck === currentDay) {
-            return $scope.events[i].status;
-          }
-        }
-      }
-
-      return '';
-    };
+    $scope.daySelect = function($event) {
+      console.log($event);
+    }
   }]);
---
+++ frontend/app/scripts/directives/trnEntry.js
@@ -0,0 +1,78 @@
+'use strict';
+
+angular.module('frontendApp')
+  .directive('trnEntry', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'views/templates/dateSelect.html',
+      scope: true,
+      controller: ['$scope', function($scope) {
+
+        $scope.today = function() {
+          //$scope. = new Date();
+        };
+        $scope.today();
+
+        $scope.clear = function () {
+          //$scope. = null;
+        };
+        console.log($scope.ngModel);
+
+
+        $scope.toggleMin = function() {
+          $scope.minDate = $scope.minDate ? null : new Date();
+        };
+        $scope.toggleMin();
+
+        $scope.open = function($event) {
+          $event.preventDefault();
+          $event.stopPropagation();
+
+          $scope.opened = true;
+        };
+
+        $scope.dateOptions = {
+          formatYear: 'yy',
+          startingDay: 1
+        };
+
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        $scope.format = $scope.formats[0];
+
+        var tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        var afterTomorrow = new Date();
+        afterTomorrow.setDate(tomorrow.getDate() + 2);
+        $scope.events =
+          [
+            {
+              date: tomorrow,
+              status: 'full'
+            },
+            {
+              date: afterTomorrow,
+              status: 'partially'
+            }
+          ];
+
+        $scope.getDayClass = function(date, mode) {
+          if (mode === 'day') {
+            var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+            for (var i=0;i<$scope.events.length;i++){
+              var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+              if (dayToCheck === currentDay) {
+                return $scope.events[i].status;
+              }
+            }
+          }
+
+          return '';
+        };
+      }],
+      link: function(scope, element, attrs) {
+
+      }
+    }
+  });
