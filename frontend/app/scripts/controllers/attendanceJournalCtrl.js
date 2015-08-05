38951729593cf690d2fe84b8442555cdc449d284
Natallia Kiraikova <n.kozlova@gmail.com>
2015-08-05 17:45:49 +0300

    Attendance Journal. Data is displayed without editing.

--- frontend/app/scripts/controllers/attendanceJournalCtrl.js
+++ frontend/app/scripts/controllers/attendanceJournalCtrl.js
@@ -1,25 +1,70 @@
-angular.module('frontendApp').controller('AttendanceJournalCtrl', ['$scope', 'training', function ($scope, training) {
-
-  training.getEntries({id: $scope.trainingId}, function (data) {
-    data.forEach(function (entry) {
-      entry.beginTime = new Date(Number(entry.beginTime));
-    });
-    $scope.entries = angular.copy(data);
-
-  });
-
-  training.getParticipants({id: $scope.trainingId}, function (data) {
-    $scope.participants = angular.copy(data);
-  });
-
-  training.getParticipation({id: $scope.trainingId}, function (data) {
-    $scope.participation = angular.copy(data);
-  });
+angular.module('frontendApp').controller('AttendanceJournalCtrl', ['$scope', '$q', 'training', function ($scope, $q, training) {
 
   //TODO user real dates here
-  // var _beginDate = 1432501200000;
-  //var _endDate = 1432846800000;
-  // training.getAbsentees({id: $scope.trainingId, beginDate: _beginDate, endDate: _endDate}, function(data){
-  // $scope.absentees = angular.copy(data);
-  //});
+  var _beginDate = 1430450800000;
+  var _endDate = 1431939600000;
+
+  var ABSENT = 'н';
+  var PRESENT = 'б';
+  var NOT_ATTEND = 'x';
+
+  $q.all({
+    participants: training.getParticipants({id: $scope.trainingId}).$promise,
+    participation: training.getParticipation({id: $scope.trainingId}).$promise,
+    entries: training.getAbsentees({id: $scope.trainingId, beginDate: _beginDate, endDate: _endDate}).$promise
+  }).then(function (values) {
+    var participants = values.participants;
+    var participation = values.participation;
+    var entries = values.entries;
+
+    //sort by date
+    entries.sort(function (a, b) {
+      var c = new Date(a.beginTime);
+      var d = new Date(b.beginTime);
+      return c - d;
+    });
+
+    var ajArray = [];
+
+    participants.forEach(function (participant) {
+      var newUserRow = {};
+      newUserRow.userName = participant.name + " " + participant.surname;
+
+      var newUserAttendanceArray = [];
+
+      entries.forEach(function (entry) {
+        //by default suppose, that the user was present
+        var entryAttendance = {entryId: entry.id, state: PRESENT};
+
+        //check whether the user was registered to training for this entry
+        for (var i = 0; i < participation.length; i++) {
+          if (participation[i].userId == participant.id) {
+            if (participation[i].beginDay > entry.beginTime || participation[i].endDay < entry.beginTime) {
+              entryAttendance.state = NOT_ATTEND;
+            }
+            break;
+          }
+        }
+
+        //check list with absentees for this entry
+        var absenteesForEntry = entry.absentees;
+        for (var j = 0; j < absenteesForEntry.length; j++) {
+          if (absenteesForEntry[j].userId == participant.id) {
+            //found that user was absent
+            entryAttendance.state = ABSENT;
+            entryAttendance.reason = absenteesForEntry[j].reason;
+            break;
+          }
+        }
+        newUserAttendanceArray.push(entryAttendance);
+      });
+      newUserRow.attendance = newUserAttendanceArray;
+      ajArray.push(newUserRow);
+    });
+
+    $scope.entries = entries;
+    $scope.ajArray = ajArray;
+  });
+
 }]);
+
